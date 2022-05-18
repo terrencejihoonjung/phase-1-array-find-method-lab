10c9819bc9ebb9e9885f376d06c2e2d02648143a
--replace-all <terrencejung928@gmail.com>
2022-05-18 16:11:50 -0700

    commit tree e24fb13d26091f7b6503369d7d74d3ef5e2d5a94

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(record) {
+    let won = record.find(observeResult);
+    if (won === undefined) {
+        return won;
+    }
+    return won.year;
+}
+
+function observeResult(season) {
+    return season.result === 'W';
+    
+}
